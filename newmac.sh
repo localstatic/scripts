f9c736c5a0ae8ff9870a24ef40c8dd3d22eb525d
Morgan Terry <localstatic@localstatic.com>
2015-07-09 00:09:02 -0600

    Add script for setting up a basic environment on a clean Mac OSX install

---
+++ newmac.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+echo
+echo
+echo
+echo "This script will install some of Morgan's favorite software. It'll be awesome."
+echo "If you don't blindly trust him, you may want to edit this script before running it."
+echo
+echo "For App Store stuff, log in and install them via the Purchases tab."
+echo
+echo "Some of the stuff installed by this script requires Xcode, so be sure to install that before continuing."
+echo
+echo
+read -p "press Enter to begin"
+
+# ========== #
+
+echo "Installing Homebrew ..."
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+
+# -------
+
+echo "Installing MacVim ..."
+
+brew install macvim
+brew linkapps macvim
+
+# -------
+
+echo "Installing cask: iTerm2 ..."
+
+brew cask install --appdir="/Applications" iterm2
+
+# -------
+
+echo "Installing cask: Google Chrome ..."
+
+brew cask install --appdir="/Applications" google-chrome
+
+# -------
+
+echo "Installing cask: Mozilla Firefox ..."
+
+brew cask install --appdir="/Applications" firefox
+
+# -------
+
+echo "Setting up basic user configuration ..."
+
+if [ ! -e ~/.ssh/id_rsa ]; then
+	echo "Generating SSH key ..."
+	ssh-keygen
+	cat ~/.ssh/id_rsa.pub | pbcopy
+	echo "Your SSH public key has been copied to the clipboard. Add it to your GitHub account before continuing."
+	read -p "press Enter to continue"
+fi
+
+if [ ! -d ~/Configs ]; then
+	mkdir ~/Configs
+fi
+
+# -------
+
+echo "Setting up localstatic/dotfiles ..."
+
+brew tap thoughtbot/formulae
+brew install rcm
+
+git clone git@github.com:localstatic/dotfiles.git ~/Configs/dotfiles
+if [ ! -e ~/.dotfiles ]; then
+	ln -s ~/Configs/dotfiles ~/.dotfiles
+fi
+rcup -x README.md -x rcrc.template
+
+# -------
+
+echo "Configuring MacVim using localstatic/vimrc ..."
+
+git clone git@github.com:localstatic/vimrc.git ~/Configs/vimrc
+~/Configs/vimrc/setup.sh
+
+# -------
+
+echo "Finished."
+

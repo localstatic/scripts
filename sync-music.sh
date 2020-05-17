32199f1cf317212567733d096759ca5db5a2a03d
Morgan Terry <morgan@localstatic.com>
2020-05-17 17:22:26 -0600

    Add script for syncing music library to alternate location
    This uses a config file (`~/.sync-music.conf`) that should be
    `source`able and contain `SOURCE` and `DESTINATION` variables
    suitable for use as the respective arguments to `rsync`

---
+++ sync-music.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CONF="${HOME}/.sync-music.conf"
+if [[ -f $CONF ]]; then
+  source $CONF
+fi
+
+if [[ -z $SOURCE || -z $DESTINATION ]]; then
+  echo "SOURCE and DESTINATION are both required and should be suitable for use by \`rsync\`.\nCheck config file (${CONF})."
+  exit 1
+fi
+
+echo "Syncing from ${SOURCE} to ${DESTINATION}."
+echo "Proceed? (y/N)"
+read -n 1
+
+if [[ $REPLY != 'y' && $REPLY != 'Y' ]]; then
+  echo "Aborted by user."
+  exit
+fi
+
+tmpfile=$(mktemp -t sync-music)
+if [[ $? -gt 0 ]]; then
+  echo "Unable to create temp file for sync output."
+  exit
+fi
+
+rsync -av --delete "${SOURCE}" "${DESTINATION}" 2>&1 | tee $tmpfile
+
+echo
+echo "Sync complete. Results saved to ${tmpfile}"
+

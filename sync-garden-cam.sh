ed86237bd00c2df5fb5f4a9b634fc3dfe2adf41b
Morgan Terry <morgan@localstatic.com>
2021-08-17 16:27:27 -0600

    feat: temporarily disable timelapse functionality

--- sync-garden-cam.sh
+++ sync-garden-cam.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 GARDEN_PATH="${HOME}/garden"
-TIMELAPSE_SHARE_PATH="${HOME}/Nextcloud/Media/garden-timelapse-fast.mp4"
+# TIMELAPSE_SHARE_PATH="${HOME}/Nextcloud/Media/garden-timelapse-fast.mp4"
 
 rsync -avr pi@garden:/home/pi/camera-images/ "${GARDEN_PATH}/images"
 
-ffmpeg -y -r 30 -pattern_type glob -i "${GARDEN_PATH}/images/*:[03]0:??-06:00.jpg" -s hd1080 -vcodec libx264 "${GARDEN_PATH}/timelapse-fast.mp4"
-ffmpeg -y -r 15 -pattern_type glob -i "${GARDEN_PATH}/images/*.jpg" -s hd1080 -vcodec libx264 "${GARDEN_PATH}/timelapse.mp4"
+# ffmpeg -y -r 30 -pattern_type glob -i "${GARDEN_PATH}/images/*:[03]0:??-06:00.jpg" -s hd1080 -vcodec libx264 "${GARDEN_PATH}/timelapse-fast.mp4"
+# ffmpeg -y -r 15 -pattern_type glob -i "${GARDEN_PATH}/images/*.jpg" -s hd1080 -vcodec libx264 "${GARDEN_PATH}/timelapse.mp4"
 
-cp "${GARDEN_PATH}/timelapse-fast.mp4" "${TIMELAPSE_SHARE_PATH}"
+# cp "${GARDEN_PATH}/timelapse-fast.mp4" "${TIMELAPSE_SHARE_PATH}"
